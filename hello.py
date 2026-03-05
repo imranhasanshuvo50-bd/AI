808ca64af0fb8b12f933a0fea752462359852e6a
imranhasanshuvo50 <imranhasanshuvo5@gmail.com>
2026-03-05 20:12:48 +0600

    all 1st

---
+++ hello.py
@@ -0,0 +1,9 @@
+print ("jiji")
+course = "python for beginners"
+print("3: " +course.upper())
+print(course.lower())
+print("5 :" + str(course.find('for')))
+print(course.replace('for', '4'))
+print('python' in course)   
+print('pefeyth' not in course)
+
